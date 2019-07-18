5952756bbb6cc5f1652db384f1186ea791f35c5a
uthraa98 <52895609+uthraa98@users.noreply.github.com>
2019-07-19 00:08:46 +0530

    Add files via upload

---
+++ pro029.py
@@ -0,0 +1,16 @@
+def digSum(n): 
+    sumi = 0; 
+    re = 0; 
+    while(n): 
+        re = n % 10; 
+        sumi = sumi + re; 
+        n = int(n / 10);
+    return sumi; 
+def findX(n): 
+    for i in range(n + 1): 
+        if (i + digSumi(i) == n): 
+            return i; 
+    return 0; 
+n = int(input());
+print("1")
+print(findX(n)); 
