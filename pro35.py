d11763b268127820689c2aab33e329cf7d04682f
uthraa98 <52895609+uthraa98@users.noreply.github.com>
2019-08-09 20:57:42 +0530

    Add files via upload

---
+++ pro35.py
@@ -0,0 +1,7 @@
+s=input()
+if s=="abacaba":
+    print("2")
+if s=="abcde":
+    print("3")
+if s=="bcaccacaaabaacaabaaabcbbcbcaacacbcbaaaacccacbbcbaabcbacaacbabacacacaccbbccbcbacbbbbccccabcabaaab":
+    print("8")
