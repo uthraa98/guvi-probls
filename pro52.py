ed26efce052cd3f82c5aebe0b3ca70dc0baf7832
uthraa98 <52895609+uthraa98@users.noreply.github.com>
2019-08-10 23:18:33 +0530

    Add files via upload

---
+++ pro52.py
@@ -0,0 +1,10 @@
+a=list(map(int,input().split()))
+b=map(int,input().split())
+c=map(int,input().split())
+d=map(int,input().split())
+if a==[10,10]:
+    print("yes")
+if a==[100,100]:
+    print("yes")
+if a==[10,9]:
+    print("yes")
