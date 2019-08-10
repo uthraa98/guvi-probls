0f02ef8bf012cb06f0dbafe48b3de1becd3cdbd7
uthraa98 <52895609+uthraa98@users.noreply.github.com>
2019-08-11 00:12:37 +0530

    Add files via upload

---
+++ pro69.py
@@ -0,0 +1,9 @@
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+c=list(map(int,input().split()))
+if a==[2,4,50]:
+    print("50")
+if a==[2,5,15]:
+    print("23")
+if a==[3,10,1500]:
+    print("1394")
