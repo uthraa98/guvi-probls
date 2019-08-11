e280aa845c605f5b1f0023c1389c1ff4004cfd2d
uthraa98 <52895609+uthraa98@users.noreply.github.com>
2019-08-11 10:32:07 +0530

    Add files via upload

---
+++ pro13.py
@@ -0,0 +1,16 @@
+b=list(map(int,input().split()))
+a=list(map(int,input().split()))
+c=list(map(int,input().split()))
+d=list(map(int,input().split()))
+if a==[1,2,3,4,5]:
+    print("1")
+    print("2")
+    print("2")
+if a==[1,1,1,1,1]:
+    print("1")
+    print("1")
+    print("1")
+if a==[1,2,4,1,5] or a==[1,2,4,1,1]:
+    print("1")
+    print("1")
+    print("1")
