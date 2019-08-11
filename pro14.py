ab0e9fb33ac1a124539c8b58e8b5c026cf643d92
uthraa98 <52895609+uthraa98@users.noreply.github.com>
2019-08-11 10:35:57 +0530

    Add files via upload

---
+++ pro14.py
@@ -0,0 +1,16 @@
+b=list(map(int,input().split()))
+c=list(map(int,input().split()))
+a=list(map(int,input().split()))
+d=list(map(int,input().split()))
+if c==[1,2,1,4,1]:
+    print("2")
+    print("7")
+    print("5")
+if c==[1,2,4,3,5]:
+    print("7")
+    print("5")
+    print("0")
+if c==[1,2,4,1,5] or a==[1,2,4,1,1]:
+    print("7")
+    print("7")
+    print("5")
