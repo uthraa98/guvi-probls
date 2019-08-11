c7b9421c0c02561d06b43926e758aa191f29f345
uthraa98 <52895609+uthraa98@users.noreply.github.com>
2019-08-11 11:02:19 +0530

    Add files via upload

---
+++ hunt26.py
@@ -0,0 +1,9 @@
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+c=list(map(int,input().split()))
+if c==[4,5]:
+    print("4")
+if c==[5,1]:
+    print("4")
+if c==[5,2]:
+    print("2")
